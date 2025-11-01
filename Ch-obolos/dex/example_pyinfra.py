237d6611f9a69464ab098aff937c56f8ee9e0e2e
Caio Rocha <232001371@aluno.unb.br>
2025-11-01 15:41:20 -0300

    commenting

--- Ch-obolos/dex/example_pyinfra.py
+++ Ch-obolos/dex/example_pyinfra.py
@@ -17,11 +17,14 @@ def fetch_local_packages():
 
     try:
         # 1. Criar Inventário com suporte à API nova do pyinfra
+        # Necessary step, creates the inventory with the IPs
         inventory = Inventory(
-            names_data=(["@local"], {}),    # dados de nomes não usados aqui
+            names_data=(["@local"], {}),
         )
 
         # 2. Criar configuração e o estado, incluindo o conector local
+        # Necessary steps, makes config an alias+allows to use connectors as an localhost
+        # then, on the state, it passas the connections and the connectors
         config = Config()
         config.connectors = {"@local": (LocalConnector, {})}
         state = State(
@@ -31,15 +34,18 @@ def fetch_local_packages():
 
         # 3. Conectar ao host
         print("Conectando ao host local...")
+        # Necessary step, connects with the config in state
         connect.connect_all(state)
 
         # 4. Obter o host do inventário
+        # Necessary step, uses the localhost as an host
         host = state.inventory.get_host("@local")
 
         # 5. Coletar dados via 'pacman -Qqen'
         print("Coletando pacotes instalados (via pacman -Qqen)...")
 
-        raw_output_string = host.get_fact(Command, "LC_ALL=C pacman -Qqen")
+        # This allows for quick command inputs
+        raw_output_string = host.get_fact(Command, "pacman -Qqen")
 
         if not raw_output_string:
             print("Nenhum pacote encontrado.")
@@ -48,16 +54,14 @@ def fetch_local_packages():
         # 6. Exibir resultado
         print(f"\n--- Pacotes Nativos e Explícitos (-Qqen) ---")
 
-        # TRANSFORMAR A STRING EM LISTA:
-        # .strip() remove newlines extras no início/fim
-        # .splitlines() quebra a string em uma lista de linhas
+        # creates the parsed list
         package_list = raw_output_string.strip().splitlines()
 
-        # Agora 'package_list' é uma lista ['pkg1', 'pkg2', ...]
-        # E podemos ordená-la
+        # sorts list
         package_list.sort()
 
         print(f"Total: {len(package_list)}\n")
+        # prints list
         for pkg_name in package_list:
             print(pkg_name)
 
@@ -68,6 +72,7 @@ def fetch_local_packages():
     finally:
         if state:
             print("\nDesconectando...")
+            # Necessary step, disconnection
             connect.disconnect_all(state)
             print("Conexão finalizada.")
 
