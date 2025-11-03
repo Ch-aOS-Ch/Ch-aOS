37d9c345072dd42440b5dad6d03fdbab598c1d97
Caio Rocha <232001371@aluno.unb.br>
2025-11-03 13:20:42 -0300

    debug mode, dry run, yes mode and readme update

--- main.py
+++ main.py
@@ -21,16 +21,43 @@ def main():
     parser = argparse.ArgumentParser(description="Pyinfra Ch-aronte orquestrator.")
     parser.add_argument('tags', nargs='+', help="The tag(s) for the role(s) to be executed(ex: pkgs, users).")
     parser.add_argument('-e', '--chobolo', required=True, help="Path to Ch-obolo to be used.")
+    parser.add_argument('-ikwid', '-y', '--i-know-what-im-doing', action='store_true', help="I Know What I'm Doing mode, basically skips confirmations, only leaving sudo calls")
+    parser.add_argument('--dry', '-d', action='store_true', help="Execute in dry mode.")
+    parser.add_argument('-v', action='count', default=0, help="Increase verbosity level. -v for WARNING, -vvv for DEBUG.")
+    parser.add_argument('--verbose', type=int, choices=[1, 2, 3], help="Set log level directly. 1=WARNING, 2=INFO, 3=DEBUG.")
     args = parser.parse_args()
-    chobolo_path = args.chobolo
 
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+    log_level = None
+    if args.verbose:
+        if args.verbose == 1:
+            log_level = logging.WARNING
+        elif args.verbose == 2:
+            log_level = logging.INFO
+        elif args.verbose == 3:
+            log_level = logging.DEBUG
+    elif args.v == 1:
+        log_level = logging.WARNING
+    elif args.v == 2:
+        log_level = logging.INFO
+    elif args.v == 3:
+        log_level = logging.DEBUG
+
+    if log_level:
+        logging.basicConfig(level=log_level, format="%(levelname)s: %(message)s")
+
+    chobolo_path = args.chobolo
+    ikwid = args.i_know_what_im_doing
+    dry = args.dry
+    drySkip = dry
+
     hosts = ["@local"]
     inventory = Inventory((hosts, {}))
     config = Config()
     state = State(inventory, config)
     state.current_stage = StateStage.Prepare
     ctx_state.set(state)
+
+    skip = ikwid
 
     print("Connecting to localhost...")
     connect_all(state)
@@ -43,10 +70,10 @@ def main():
         normalized_tag = ROLE_ALIASES.get(tag,tag)
         if normalized_tag in ROLES_DISPATCHER:
                 print(f"\n--- Executing {normalized_tag} role with Ch-obolo: {chobolo_path} ---")
-                ROLES_DISPATCHER[normalized_tag](state, host, chobolo_path)
-                print(f"--- '{normalized_tag}' role finalized. ---")
+                ROLES_DISPATCHER[normalized_tag](state, host, chobolo_path, skip, drySkip)
+                print(f"\n--- '{normalized_tag}' role finalized. ---")
         else:
-            logging.warning(f"Unknown tag '{normalized_tag}'. Skipping.")
+            print(f"\nWARNING: Unknown tag '{normalized_tag}'. Skipping.")
 
     # --- Desconexão ---
     print("\nDisconnecting...")
