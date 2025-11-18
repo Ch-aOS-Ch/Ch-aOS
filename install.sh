8f5d242061f47fcadd5613fd540883027c1c7e15
Caio Rocha <232001371@aluno.unb.br>
2025-11-17 21:14:10 -0300

    pex, i love you

--- install.sh
+++ install.sh
@@ -1,10 +1,1 @@
-pyinstaller --onefile src/charonte/cli.py \
-            --name B-coin \
-            --copy-metadata charonte \
-            --paths src \
-            --collect-all charonte \
-            --collect-all pyinfra.operations \
---collect-all passlib \
---collect-all omegaconf \
---collect-all pyinfra.connectors \
---collect-all pyinfra.facts
+pex . -c B-coin -o dist/B-coin --inherit-path=fallback
