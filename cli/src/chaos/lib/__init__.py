42f1df29ee7de262cc27ca6e3a26618f254ef955
Caio Rocha de Oliveira <caiorocoli@gmail.com>
2026-01-20 21:41:55 -0300

    ye

---
+++ cli/src/chaos/lib/__init__.py
@@ -0,0 +1,10 @@
+"""
+Within every building made with pride,
+The architect remains\istrapped\islocked inside.
+
+But there is nothing an artist can do.
+When you swing your brush you have to follow through.
+
+Fleeting paint, a shattered hart
+that's what makes a work of art.
+"""
