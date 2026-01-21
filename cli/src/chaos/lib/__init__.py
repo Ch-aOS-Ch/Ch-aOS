1836317b9ae3150b64979c571270c9be9c03b601
Caio Rocha de Oliveira <caiorocoli@gmail.com>
2026-01-20 22:47:19 -0300

    ye

--- cli/src/chaos/lib/__init__.py
+++ cli/src/chaos/lib/__init__.py
@@ -1,10 +1,24 @@
 """
-Within every building made with pride,
-The architect remains\istrapped\islocked inside.
+Within every (building|story) made with pride,
+The architect remains\istrapped\islocked (inside|beside).
 
 But there is nothing an artist can do.
 When you swing your brush you have to follow through.
 
 Fleeting paint, a wounded hart
-that's what makes a work of art.
+(t)hat's what makes a work of art.
+
+Such escapes which stay and fade away,
+(T)he (a)rchitect remains\rests\lives\stays that (d|w)ay.
+
+(Draining|Fleeting) paint, a shattered heart
+That's what makes a work of art.
+
+(Guiding|Helping), the (A)rtist's hand
+(T)he (A)chitect fills in the b?land.
+
+(Filling|Draining) paint, a (healing|wouding) hart
+That's what makes a (Building|Work of Art).
 """
+
+"IDK, i'm not a poet"
